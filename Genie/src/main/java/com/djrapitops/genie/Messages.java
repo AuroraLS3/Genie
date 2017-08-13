cb96087dce7b22034124fe2e0d01964837a699f3
Rsl1122 <djrapitops@hotmail.com>
2017-08-13 10:16:40 +0300

    Clean up the first PR

--- Genie/src/main/java/com/djrapitops/genie/Messages.java
+++ Genie/src/main/java/com/djrapitops/genie/Messages.java
@@ -1,5 +1,6 @@
 package com.djrapitops.genie;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.Map;
 import java.util.Random;
 
 import com.djrapitops.genie.file.MessageHandler;
+import org.bukkit.configuration.InvalidConfigurationException;
 
 /**
  * Class that contains message parts and variations to make genie more live
@@ -18,11 +20,18 @@ public class Messages {
 
     private final Map<MessageType, List<String>> messages;
     private final Random random;
+    private MessageHandler msgHandler;
 
     public Messages() {
         messages = new HashMap<>();
         for (MessageType type : MessageType.values()) {
             messages.put(type, new ArrayList<>());
+        }
+        try {
+            msgHandler = new MessageHandler(Genie.getInstance());
+        } catch (IOException | InvalidConfigurationException e) {
+            Log.error("Failed to initialize Messages.");
+            Log.toLog(this.getClass().getName(), e);
         }
         random = new Random();
         addMessages();
@@ -41,134 +50,30 @@ public class Messages {
         messages.get(type).add(msg);
     }
 
-    MessageHandler Mhandler = new MessageHandler(Genie.getInstance());
+
     private void addMessages() {
-    	if (!Mhandler.exists()){
-    		Mhandler.setupfile();
-    		
-    		//Summon Messages   		
-    		List<String> summonMlist = new ArrayList<String>();
-    		summonMlist.add("Behold, you have summoned me, Genie!");
-    		summonMlist.add("Behold, I am the Genie of the lamp!");
-    		summonMlist.add("Look at me I'm Mr. MEESEEKS!");
-    		summonMlist.add("BEWARE THE WRATH OF THE OLD GENIE, Just kiddin'");
-    		summonMlist.add("Thanks for letting me out of there!");
-    		summonMlist.add("Thank yaa for freeing me from me lamp eh!");
-    		summonMlist.add("I have slept for thousands of years, but now you woke me from my nap!");
-    		summonMlist.add("What if I told you, that I am a Genie?");
-    		summonMlist.add("Thank you for helping me with my geometry homework.");
-    		summonMlist.add("Ok, ok, you released me from this lamp bla bla bla.");
-    		summonMlist.add("How about another wish to help your situation?");
-    		summonMlist.add("Who has called the BUTT GENIE?");
-    		summonMlist.add("SHAZAM!");
-    		summonMlist.add("KAZAAM!");
-    		summonMlist.add("Alright Sparky, here's the deal.");
-    		summonMlist.add("Poof! Waddya need?");
-    		Mhandler.addMessages(summonMlist,"Summon-Messages");
-    		
-    		
-    		
-    		//Fulfill Messages    		
-    		List<String> fulfillMlist = new ArrayList<String>();
-    		fulfillMlist.add("Your word is my command!");
-    		fulfillMlist.add("Here you go");
-    		fulfillMlist.add("The genie has granted your wish");
-    		fulfillMlist.add("How about them apples?");
-    		fulfillMlist.add("I think you might have wanted this");
-    		fulfillMlist.add("This is the best I can do");
-    		fulfillMlist.add("Is this enough?");
-    		fulfillMlist.add("How about this, do you think it'll do?");
-    		fulfillMlist.add("Maybe you want this and don't know it yet.");
-    		fulfillMlist.add("Applause please!");
-    		Mhandler.addMessages(fulfillMlist,"Fulfill-Messages");
-    	   
+        for (String sm : msgHandler.getSummonMessages()) {
+            addMsg(MessageType.SUMMON, sm);
+        }
 
-    		//Wish help messages
-    		List<String> helpMlist = new ArrayList<String>();
-    		helpMlist.add("Hold the lamp & speak your wish!");
-    		helpMlist.add("Hold the lamp & speak thy wish!");
-    		helpMlist.add("Hold that lamp & let me know your wish");
-    		helpMlist.add("Tell me your wish");
-    		helpMlist.add("How about a wish?");
-    		helpMlist.add("I'd like to hear your wish, just hold the lamp.");
-    		helpMlist.add("Hold the lamp while speaking.");
-    		helpMlist.add("Bla bla bla.");
-    		helpMlist.add("Speak your mind, release your wish.");
-    		helpMlist.add("You only had to rub the lamp, but I appreciate you going the extra mile.");
-    		Mhandler.addMessages(helpMlist,"Help-Messages");
-    		
-    		
-    		//Wishes remaining messages
-    		List<String> wishesleftMlist = new ArrayList<String>();
-    		wishesleftMlist.add("You have WISHES wishes left.");
-    		wishesleftMlist.add("I can still grant WISHES wishes.");
-    		wishesleftMlist.add("WISHES wishes to go.");
-    		wishesleftMlist.add("How many wishes left? WISHES");
-    		wishesleftMlist.add("About WISHES wishes..");
-    		wishesleftMlist.add("You still got WISHES wishes left.");
-    		wishesleftMlist.add("I can grant you WISHES wishes.");
-    		wishesleftMlist.add("WISHES wishes left.");
-    	    	
-    		Mhandler.addMessages(wishesleftMlist,"Wishes-Remaining-Messages");
-    		
-    		
-    		
-    		//No wishes messages
-    		List<String> NowishMlist = new ArrayList<String>();
-    		NowishMlist.add("No wishes left.");
-    		NowishMlist.add("This lamp has no wishes.");
-    		NowishMlist.add("Zero, Nada, Non' - Wishes left.");
-    		NowishMlist.add("I'm afraid it's my time to go kids, this lamp is out of wishes.");
-    		NowishMlist.add("You used all of your wishes.");
-    		NowishMlist.add("I'm all out of wishes.");
-    		NowishMlist.add("You're out of wishes.");
-    		NowishMlist.add("0 wishes left.");
-    		NowishMlist.add("That was your last wish.");
-    		NowishMlist.add("Excuse me, I am in the shower. I think you used your wishes already.");
-    		Mhandler.addMessages(NowishMlist,"Out-Of-Wishes-Messages");
-    		
-    		
-    		//Cannot find wish messages
-    		List<String> cannotMlist = new ArrayList<String>();
-    		cannotMlist.add("Gadzooks! I don't think that can be done, have another try.");
-    		cannotMlist.add("Sorry, I do not know how to fulfill your wish.");
-    		cannotMlist.add("Sorry, I do not know how to fulfill that wish.");
-    		cannotMlist.add("Could you think of something less impossible?");
-    		cannotMlist.add("That may be beyond my capabilities");
-    		cannotMlist.add("Please ask for the dev to add that to my abilities. ");
-    		Mhandler.addMessages(cannotMlist,"Cannot-Fulfill-Messages");
-    		
-    	}
-    	
-    	
-    	
-    
-     
-    	for (String sm : Mhandler.getSummonMessages()){
-    		addMsg(MessageType.SUMMON,sm);
-    	}
-    	
-    	for (String sm : Mhandler.getFulfillMessages()){
-    		addMsg(MessageType.FULFILL,sm);
-    	}
-    	
-    	for (String sm : Mhandler.getHelpMessages()){
-    		addMsg(MessageType.HELP_WISH,sm);
-    	}
-     
-    	for (String sm : Mhandler.getWishesLeftMessages()){
-    		addMsg(MessageType.WISHES_LEFT,sm);
-    	}
-       
-        for (String sm : Mhandler.getNoWishesMessages()){
-    		addMsg(MessageType.OUT_OF_WISHES,sm);
-    	}
-     
-        
-        for (String sm : Mhandler.getCannotFulfillMessages()){
-    		addMsg(MessageType.CANNOT_FIND,sm);
-    	}
-      
+        for (String sm : msgHandler.getFulfillMessages()) {
+            addMsg(MessageType.FULFILL, sm);
+        }
+
+        for (String sm : msgHandler.getHelpMessages()) {
+            addMsg(MessageType.HELP_WISH, sm);
+        }
+
+        for (String sm : msgHandler.getWishesLeftMessages()) {
+            addMsg(MessageType.WISHES_LEFT, sm);
+        }
+
+        for (String sm : msgHandler.getNoWishesMessages()) {
+            addMsg(MessageType.OUT_OF_WISHES, sm);
+        }
+
+        for (String sm : msgHandler.getCannotFulfillMessages()) {
+            addMsg(MessageType.CANNOT_FIND, sm);
+        }
     }
-
 }
--- Genie/src/main/java/com/djrapitops/genie/file/MessageHandler.java
+++ Genie/src/main/java/com/djrapitops/genie/file/MessageHandler.java
@@ -1,99 +1,144 @@
 package com.djrapitops.genie.file;
 
+import com.djrapitops.genie.Genie;
+import com.djrapitops.genie.Log;
+import com.djrapitops.plugin.config.BukkitConfig;
+import com.djrapitops.plugin.utilities.Verify;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
+public class MessageHandler extends BukkitConfig<Genie> {
 
-import com.djrapitops.genie.Genie;
+    private File messageFile;
+    private FileConfiguration msgConfig;
 
-public class MessageHandler {
-	
-	
-	private final Genie plugin;
-	 File messagefile;
-	 
-	  public MessageHandler(Genie plugin) {
-	        this.plugin = plugin;
-	    }
-	  YamlConfiguration Mconfig;
-	  
-	  
-	  public void addMessages(List<String> messages,String section){
-		  messagefile = new File(plugin.getDataFolder().getAbsolutePath()+"/" + "messages.yml").getAbsoluteFile();
-		  Mconfig = YamlConfiguration.loadConfiguration(messagefile);
-		  List<String> messagelist = Mconfig.getConfigurationSection("Messages").getStringList(section);
-			  messagelist.addAll(messages);
-			 Mconfig.getConfigurationSection("Messages").set(section, (List<String>)messagelist);
-		  try {
-			Mconfig.save(messagefile);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		  
-	  }
-	  
-	  
-	  public void setupfile(){
-		  try {
-				new File(plugin.getDataFolder().getAbsolutePath()+"/" + "messages.yml").createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			messagefile = new File(plugin.getDataFolder().getAbsolutePath()+"/" + "messages.yml").getAbsoluteFile();
-			 Mconfig = YamlConfiguration.loadConfiguration(messagefile);
-			 Mconfig.createSection("Messages");
+    public MessageHandler(Genie plugin) throws IOException, InvalidConfigurationException {
+        super(plugin, "messages.yml");
+        super.createFile();
+        FileConfiguration config = super.getConfig();
+        config.options().copyDefaults(true);
+        addDefaults(config);
+        save();
+    }
 
-		
-			try {
-				Mconfig.save(messagefile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	  }
-	  
-	  private ConfigurationSection getMessagesSection() {
-		  messagefile = new File(plugin.getDataFolder().getAbsolutePath()+"/" + "messages.yml").getAbsoluteFile();
-	        Mconfig = YamlConfiguration.loadConfiguration(messagefile);
-	        return Mconfig.getConfigurationSection("Messages");
-	  }
-	  
-	  
-	  public ArrayList<String> getSummonMessages(){
-		ConfigurationSection section = getMessagesSection();
-		return new ArrayList<String>(section.getStringList("Summon-Messages"));
-	  }
-	  
-	  public ArrayList<String> getFulfillMessages(){
-			ConfigurationSection section = getMessagesSection();
-			return new ArrayList<String>(section.getStringList("Fulfill-Messages"));
-		  }
-	  
-	  public ArrayList<String> getHelpMessages(){
-			ConfigurationSection section = getMessagesSection();
-			return new ArrayList<String>(section.getStringList("Help-Messages"));
-		  }
-	  
-	  public ArrayList<String> getNoWishesMessages(){
-			ConfigurationSection section = getMessagesSection();
-			return new ArrayList<String>(section.getStringList("Out-Of-Wishes-Messages"));
-		  }
-	  
-	  public ArrayList<String> getCannotFulfillMessages(){
-			ConfigurationSection section = getMessagesSection();
-			return new ArrayList<String>(section.getStringList("Cannot-Fulfill-Messages"));
-		  }
-	  public ArrayList<String> getWishesLeftMessages(){
-			ConfigurationSection section = getMessagesSection();
-			return new ArrayList<String>(section.getStringList("Wishes-Remaining-Messages"));
-		  }
-	  
-	 public Boolean exists(){
-		 messagefile = new File(plugin.getDataFolder().getAbsolutePath()+"/" + "messages.yml").getAbsoluteFile();
-		return messagefile.exists();
-	 }
+    private void addDefaults(FileConfiguration config) {
+        List<String> summonMsgList = new ArrayList<>();
+        summonMsgList.add("Behold, you have summoned me, Genie!");
+        summonMsgList.add("Behold, I am the Genie of the lamp!");
+        summonMsgList.add("Look at me I'm Mr. MEESEEKS!");
+        summonMsgList.add("BEWARE THE WRATH OF THE OLD GENIE, Just kiddin'");
+        summonMsgList.add("Thanks for letting me out of there!");
+        summonMsgList.add("Thank yaa for freeing me from me lamp eh!");
+        summonMsgList.add("I have slept for thousands of years, but now you woke me from my nap!");
+        summonMsgList.add("What if I told you, that I am a Genie?");
+        summonMsgList.add("Thank you for helping me with my geometry homework.");
+        summonMsgList.add("Ok, ok, you released me from this lamp bla bla bla.");
+        summonMsgList.add("How about another wish to help your situation?");
+        summonMsgList.add("Who has called the BUTT GENIE?");
+        summonMsgList.add("SHAZAM!");
+        summonMsgList.add("KAZAAM!");
+        summonMsgList.add("Alright Sparky, here's the deal.");
+        summonMsgList.add("Poof! Waddya need?");
+        config.addDefault("Summon-Messages", summonMsgList);
 
+        List<String> fulfillMsgList = new ArrayList<>();
+        fulfillMsgList.add("Your word is my command!");
+        fulfillMsgList.add("Here you go");
+        fulfillMsgList.add("The genie has granted your wish");
+        fulfillMsgList.add("How about them apples?");
+        fulfillMsgList.add("I think you might have wanted this");
+        fulfillMsgList.add("This is the best I can do");
+        fulfillMsgList.add("Is this enough?");
+        fulfillMsgList.add("How about this, do you think it'll do?");
+        fulfillMsgList.add("Maybe you want this and don't know it yet.");
+        fulfillMsgList.add("Applause please!");
+        config.addDefault("Fulfill-Messages", fulfillMsgList);
+
+        List<String> helpMsgList = new ArrayList<>();
+        helpMsgList.add("Hold the lamp & speak your wish!");
+        helpMsgList.add("Hold the lamp & speak thy wish!");
+        helpMsgList.add("Hold that lamp & let me know your wish");
+        helpMsgList.add("Tell me your wish");
+        helpMsgList.add("How about a wish?");
+        helpMsgList.add("I'd like to hear your wish, just hold the lamp.");
+        helpMsgList.add("Hold the lamp while speaking.");
+        helpMsgList.add("Bla bla bla.");
+        helpMsgList.add("Speak your mind, release your wish.");
+        helpMsgList.add("You only had to rub the lamp, but I appreciate you going the extra mile.");
+        config.addDefault("Help-Messages", helpMsgList);
+
+        List<String> wishesRemainingMsgList = new ArrayList<>();
+        wishesRemainingMsgList.add("You have WISHES wishes left.");
+        wishesRemainingMsgList.add("I can still grant WISHES wishes.");
+        wishesRemainingMsgList.add("WISHES wishes to go.");
+        wishesRemainingMsgList.add("How many wishes left? WISHES");
+        wishesRemainingMsgList.add("About WISHES wishes..");
+        wishesRemainingMsgList.add("You still got WISHES wishes left.");
+        wishesRemainingMsgList.add("I can grant you WISHES wishes.");
+        wishesRemainingMsgList.add("WISHES wishes left.");
+        config.addDefault("Wishes-Remaining-Messages", wishesRemainingMsgList);
+
+        List<String> noWishesLeftMsgList = new ArrayList<>();
+        noWishesLeftMsgList.add("No wishes left.");
+        noWishesLeftMsgList.add("This lamp has no wishes.");
+        noWishesLeftMsgList.add("Zero, Nada, Non' - Wishes left.");
+        noWishesLeftMsgList.add("I'm afraid it's my time to go kids, this lamp is out of wishes.");
+        noWishesLeftMsgList.add("You used all of your wishes.");
+        noWishesLeftMsgList.add("I'm all out of wishes.");
+        noWishesLeftMsgList.add("You're out of wishes.");
+        noWishesLeftMsgList.add("0 wishes left.");
+        noWishesLeftMsgList.add("That was your last wish.");
+        noWishesLeftMsgList.add("Excuse me, I am in the shower. I think you used your wishes already.");
+        config.addDefault("Out-Of-Wishes-Messages", noWishesLeftMsgList);
+
+        List<String> cannotFulfillMsgList = new ArrayList<>();
+        cannotFulfillMsgList.add("Gadzooks! I don't think that can be done, have another try.");
+        cannotFulfillMsgList.add("Sorry, I do not know how to fulfill your wish.");
+        cannotFulfillMsgList.add("Sorry, I do not know how to fulfill that wish.");
+        cannotFulfillMsgList.add("Could you think of something less impossible?");
+        cannotFulfillMsgList.add("That may be beyond my capabilities");
+        cannotFulfillMsgList.add("Please ask for the dev to add that to my abilities. ");
+        config.addDefault("Cannot-Fulfill-Messages", cannotFulfillMsgList);
+    }
+
+    private ConfigurationSection getMessagesSection() {
+        return getConfig().getConfigurationSection("Messages");
+    }
+
+    public List<String> getSummonMessages() {
+        ConfigurationSection section = getMessagesSection();
+        return new ArrayList<>(section.getStringList("Summon-Messages"));
+    }
+
+    public List<String> getFulfillMessages() {
+        ConfigurationSection section = getMessagesSection();
+        return new ArrayList<>(section.getStringList("Fulfill-Messages"));
+    }
+
+    public List<String> getHelpMessages() {
+        ConfigurationSection section = getMessagesSection();
+        return new ArrayList<>(section.getStringList("Help-Messages"));
+    }
+
+    public List<String> getNoWishesMessages() {
+        ConfigurationSection section = getMessagesSection();
+        return new ArrayList<>(section.getStringList("Out-Of-Wishes-Messages"));
+    }
+
+    public List<String> getCannotFulfillMessages() {
+        ConfigurationSection section = getMessagesSection();
+        return new ArrayList<>(section.getStringList("Cannot-Fulfill-Messages"));
+    }
+
+    public List<String> getWishesLeftMessages() {
+        ConfigurationSection section = getMessagesSection();
+        return new ArrayList<>(section.getStringList("Wishes-Remaining-Messages"));
+    }
 }
